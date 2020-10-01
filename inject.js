db8e0c3701c2e5dca60f5076211d1fffc530896c
epic-sudo <65257768+epic-sudo@users.noreply.github.com>
2020-10-01 18:50:28 -0500

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,4 +1,4 @@
 styleElement = document.createElement('style');
 styleElement.setAttribute('type', 'text/css');
-styleElement.innerHTML = '*:not(i):not(.material-icons):not(["aria-hidden:true"]) { font-family: \'Comic Sans MS\' !important; }';
+styleElement.innerHTML = '*:not(i):not(.material-icons):not(svg):not(path):not(span) { font-family: \'Comic Sans MS\' !important; }';
 document.head.appendChild(styleElement);
